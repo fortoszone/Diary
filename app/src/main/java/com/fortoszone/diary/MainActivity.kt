a6baa0a8da32921021bdcd67ccdcce45908e0a74
Devwanto Dariel Nugroho <fortoszone@gmail.com>
2023-07-17 11:06:54 +0700

    initial commit, add dependencies

---
+++ app/src/main/java/com/fortoszone/diary/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.fortoszone.diary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.fortoszone.diary.ui.theme.DiaryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiaryTheme {
+
+            }
+        }
+    }
+}
